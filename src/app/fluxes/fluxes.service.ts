f5f6e60553bc2acc0aa26d2d559f38d51ea55c04
fpasini <fpasini@kernix.com>
2017-03-07 16:10:00 +0100

    delete comment

--- src/app/fluxes/fluxes.service.ts
+++ src/app/fluxes/fluxes.service.ts
@@ -6,45 +6,25 @@ import 'rxjs/add/operator/do';
 import 'rxjs/add/operator/catch';
 import 'rxjs/add/observable/throw';
 
-import { AuthService } from '../auth/auth.service';
 
 @Injectable()
 export class FluxesService {
 
-  constructor(protected authHttp: AuthHttp/*, protected authService: AuthService*/) {
-  }
+  constructor(protected authHttp: AuthHttp) {}
 
   getFluxesFromAPI() {
-    // this.authService.login();
-    // console.log(this.authService.authenticated());
     return this.authHttp.get('http://sf28.newsrss.net/api/user/fluxes')
-    // return this.authHttp.get('/assets/mock-api/fluxes.json')
-    // .do(x => console.log(x))
       .map(fluxes => fluxes.json())
       .catch(error => {
-        // let errorMessage = `Une erreur ${error.status} est survenue en tentant de joindre ${error.url}`;
         return Observable.throw(error);
       });
   }
 
   getItemsFromAPI() {
     return this.authHttp.get('http://sf28.newsrss.net/api/user/items')
-    // return this.authHttp.get('/assets/mock-api/items.json')
-    // .do(x => console.log(x))
       .map(items => items.json())
       .catch(error => {
         return Observable.throw(error);
       });
   }
-
-  /*
-   getItemsFromAPI() {
-   return this.authHttp.get('/assets/mock-api/items-array.json')
-   //.do(x => console.log(x))
-   .map(items => items.json())
-   .catch(error => {
-   return Observable.throw(error);
-   });
-   }
-   */
 }
