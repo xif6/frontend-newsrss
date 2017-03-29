38910b2ca2ee661bd3acbeec16191035fb57be6c
fpasini <fpasini@kernix.com>
2017-03-29 17:27:53 +0200

    update to angular v4 and angular-cli v1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,30 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
-import { HttpModule, Http, RequestOptions } from '@angular/http';
+import { HttpModule } from '@angular/http';
 
-import { AlertModule, ModalModule, DropdownModule } from 'ng2-bootstrap';
-import { AuthHttp, AuthConfig } from 'angular2-jwt';
+import { AlertModule, ModalModule, BsDropdownModule } from 'ng2-bootstrap';
 
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 
-import { AuthService } from './auth/auth.service';
-import { AuthGuardService } from './auth/auth-guard.service';
 import { AddFluxComponent } from './add-flux/add-flux.component';
 import { FluxItemsFilterPipe } from './fluxes/flux-items-filter.pipe';
 import { FluxesComponent } from './fluxes/fluxes.component';
 import { FluxesService } from './fluxes/fluxes.service';
 import { FluxComponent } from './fluxes/flux.component';
 import { AuthComponent } from './auth/auth.component';
-
-export function authHttpServiceFactory(
-  http: Http,
-  options: RequestOptions
-) {
-  return new AuthHttp(new AuthConfig(), http, options);
-}
+import { AuthModule } from './auth/auth.module';
 
 @NgModule({
   declarations: [
@@ -40,18 +31,12 @@ export function authHttpServiceFactory(
     ReactiveFormsModule,
     HttpModule,
     AppRoutingModule,
+    AuthModule,
     AlertModule.forRoot(),
     ModalModule.forRoot(),
-    DropdownModule.forRoot(),
+    BsDropdownModule.forRoot(),
   ],
   providers: [
-    {
-      provide: AuthHttp,
-      useFactory: authHttpServiceFactory,
-      deps: [Http, RequestOptions]
-    },
-    AuthService,
-    AuthGuardService,
     FluxesService
   ],
   bootstrap: [AppComponent]
